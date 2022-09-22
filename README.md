02fbf07c2644e15d50486da36cc20224700c59fc
Михаил Уткин <mixail.rasto4u@yandex.ru>
2022-09-22 15:08:04 +0400

    Добавление README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/wqug4elnf673gkpa/branch/main?svg=true)](https://ci.appveyor.com/project/UMikhail/testmode/branch/main)
--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -1,9 +1,10 @@
-
+package ru.netology.test;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.netology.data.DataGenerator;
 
 import java.time.Duration;
 
